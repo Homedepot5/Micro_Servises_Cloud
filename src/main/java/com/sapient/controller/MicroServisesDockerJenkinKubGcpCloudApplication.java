efbb15ee7b3dd02ab29b5ea1fa01fe024b64bbe8
Sunil <skumar99@sapient.com>
2019-07-25 11:05:48 +0530

    add all the spring boot file

---
+++ src/main/java/com/sapient/controller/MicroServisesDockerJenkinKubGcpCloudApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.Micro_Servises_Docker_Jenkin_Kub_GCPCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServisesDockerJenkinKubGcpCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServisesDockerJenkinKubGcpCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapient/controller/MicroServisesDockerJenkinKubGcpCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sapient.Micro_Servises_Docker_Jenkin_Kub_GCPCloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroServisesDockerJenkinKubGcpCloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
