a9ea6f2234c6c32c878f3ceec909f9f5a17ac3ea
Sunil <skumar99@sapient.com>
2019-07-26 15:18:16 +0530

    Added Controller and update build script and Main java file

--- src/main/java/com/sapient/main/MicroServisesDockerJenkinKubGcpCloudApplication.java
+++ src/main/java/com/sapient/main/MicroServisesDockerJenkinKubGcpCloudApplication.java
@@ -26,8 +26,7 @@ public class MicroServisesDockerJenkinKubGcpCloudApplication {
                 System.out.println(beanName);
             }
         };
+
     }
-
-
 
 }
--- src/main/java/com/sapient/main/com/sapient/controller/HelloController.java
+++ src/main/java/com/sapient/main/com/sapient/controller/HelloController.java
@@ -1,10 +1,11 @@
 package com.sapient.main.com.sapient.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    @RequestMapping("/")
     public String index(){
         return "Greeting from Spring Boot!";
     }
