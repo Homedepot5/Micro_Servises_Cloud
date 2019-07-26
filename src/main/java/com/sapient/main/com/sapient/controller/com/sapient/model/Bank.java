b84a49bda2792801bb9fe18f835281507c7d9326
Sunil <skumar99@sapient.com>
2019-07-26 15:50:18 +0530

    added model layer

---
+++ src/main/java/com/sapient/main/com/sapient/controller/com/sapient/model/Bank.java
@@ -0,0 +1,11 @@
+package com.sapient.main.com.sapient.controller.com.sapient.model;
+
+public class Bank {
+    int id;
+    String name;
+
+    public Bank(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
