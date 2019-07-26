31113aec42631d54a4f2f356aeb862f954c097c8
Sunil <skumar99@sapient.com>
2019-07-26 15:59:21 +0530

    Added Controller and update build script and Main java file

--- src/main/java/com/sapient/main/com/sapient/controller/com/sapient/model/Bank.java
+++ src/main/java/com/sapient/main/com/sapient/controller/com/sapient/model/Bank.java
@@ -1,11 +1,19 @@
 package com.sapient.main.com.sapient.controller.com.sapient.model;
 
 public class Bank {
-    int id;
-    String name;
+   private int id;
+    private String name;
 
     public Bank(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
